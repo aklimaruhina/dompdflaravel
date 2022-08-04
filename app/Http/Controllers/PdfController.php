43f71cfba6001674a38a1e8edf078210a94e8bc1
aklimaruhina <aklimacse10@gmail.com>
2022-08-04 09:24:26 +0600

    commit

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -12,9 +12,19 @@ class PdfController extends Controller
 	// ei controller er route konta?
     
     public function printpdf(){
+    	$mpdf = new \Mpdf\Mpdf([
+    		'default_font_size' => 12,
+    		'default_font'	=> 'nikosh'
+    	]);
     	// return view('printpdf');
-    	 $html = view('printpdf');
+    	 // $html = ;
+    	 $mpdf->writeHTML(view('printpdf'));
+    	 $mpdf->output();
     	 // return $html;
-        return @\PDF::loadHTML($html, 'utf-8')->stream();
+        // return @\PDF::loadHTML($html, 'utf-8')->stream();
+    }
+    public function pdfhtml(){
+    	$output = "আমি কোডিং ভালোবাসি";
+    	return $output;
     }
 }
